5b51c1d72c8ab393a6de3e1bdf4a035ff9c70aac
farhandwk <farhandwiki15@gmail.com>
2025-03-09 02:42:05 +0700

    asdas

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Landing from "./Components/Landing/Landing";
 import Decor from "./Components/Decor/Decor";
 import Halaman1 from "./Components/Halaman1/Halaman1";
 import Halaman2 from "./Components/Halaman2/Halaman2";
+import Halaman3 from "./Components/Halaman3/Halaman3"
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Decor></Decor>
       <Halaman1></Halaman1>
       <Halaman2></Halaman2>
+      <Halaman3></Halaman3>
     </div>
   );
 }
---
+++ src/Components/Halaman3/Halaman3.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Halaman3() {
+  return (
+    <div class="
+    w-screen h-auto
+    bg-[blue]
+    text-[white] 
+    flex flex-col
+    items-center
+    justify-start
+    ">
+        <h2
+        class="
+        HelveticaBold
+        text-[25px]
+        mt-[100px]
+        "
+        >Popular News</h2>
+    </div>
+  )
+}
+
+export default Halaman3
