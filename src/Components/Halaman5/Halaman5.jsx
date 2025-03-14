6c6db1e783b3f525e0e38366b9359c3fe3fdeaa7
farhandwk <farhandwiki15@gmail.com>
2025-03-14 12:03:42 +0700

    Halaman 5 mobile

--- src/Components/Halaman5/Halaman5.jsx
+++ src/Components/Halaman5/Halaman5.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Cards from "./Card"
+import "./Halaman5.css"
 
 function Halaman5() {
   const cards = Cards; // Use original cards array for normal scrolling
@@ -23,32 +24,58 @@ function Halaman5() {
         >Past Events</h2>
         <div
         class="
-        w-[90%]
+        card-container
+        w-[90%] h-[280px]
         flex flex-row
-        overflow-hidden 
+        overflow-hidden
+        relative
+        justify-center items-center 
         "
         >
           {cards.map((card) => (
             <div
               key={card.id}
-              className="card"
+              className="
+              card
+              grid
+              w-[200px] h-fit
+              absolute top-[0%] left-[100%]
+              "
               data-id={card.id}
               onMouseOver={() => setHoveredCardId(card.id)}
               onMouseOut={() => setHoveredCardId(null)}
               style={{ '--position': `${card.id}` }}
             >
-              <img src={card.gambar} alt={card.judul} className="card-image" />
+              <img src={card.gambar} alt={card.judul} className="card-image rounded-[10px]" />
 
 
               <div
-                className="card-content"
+                className="card-content
+                flex flex-col
+                w-full h-full
+                text-[white]
+                backdrop-blur-[10px]
+                justify-end
+                p-[7%]
+                gap-[3%]
+                rounded-[10px]
+                "
                 style={{
                   opacity: hoveredCardId === card.id ? "1" : "0",
                   transition: "0.5s",
                 }}
               >
-            <h2 className="card-title">{card.judul}</h2>
-            <div className="containerDateLoc">
+            <h2 className="card-title
+            HelveticaBold
+            text-[12px]
+            ">{card.judul}</h2>
+            <div className="containerDateLoc
+            Helvetica
+            w-full h-fit
+            flex flex-row
+            gap-[20px]
+            text-[9px]
+            ">
               <div>
                 <h3>Date</h3>
                 <p className="card-date">{card.date}</p>
